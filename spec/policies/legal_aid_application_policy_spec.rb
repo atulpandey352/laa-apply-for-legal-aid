0fc1e36f7a432895185a30b57f8a89f62fc40f70
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2020-07-10 16:45:44 +0100

    AP-1505 WIP

--- spec/policies/legal_aid_application_policy_spec.rb
+++ spec/policies/legal_aid_application_policy_spec.rb
@@ -1,22 +1,89 @@
 require 'rails_helper'
 
 RSpec.describe LegalAidApplicationPolicy do
+  # let(:passported_permission) { create :permission, :passported }
+  # let(:non_passported_permission) { create :permission, :non_passported }
+
   subject { LegalAidApplicationPolicy.new(provider, legal_aid_application) }
-  let(:legal_aid_application) { create(:legal_aid_application) }
 
-  context 'Another a provider' do
-    let(:provider) { create(:provider) }
-
-    it { should_not permit(:show) }
-    it { should_not permit(:update) }
+  let(:controller_actions) do
+    %i[
+      index
+      new
+      show
+      update
+      destroy
+      create
+      remove_transaction_type
+      continue
+      reset
+      show_submitted_application
+    ]
   end
 
-  context 'Current provider' do
-    let(:provider) do
-      legal_aid_application.provider
+  context 'Provider from another firm' do
+    let(:legal_aid_application) { create(:legal_aid_application) }
+    let(:provider) { create(:provider) }
+
+    it 'should not allow any action' do
+      controller_actions.each do |action|
+        expect(subject).not_to permit(action)
+      end
+    end
+  end
+
+  context 'provider who created the application' do
+    let(:legal_aid_application) { create(:legal_aid_application) }
+    let(:provider) { legal_aid_application.provider }
+
+    context 'application not yet checked benefits' do
+      it 'should permit all actions' do
+        controller_actions.each do |action|
+          expect(subject).to permit(action)
+        end
+      end
     end
 
-    it { should permit(:show) }
-    it { should permit(:update) }
+    context 'application is passported' do
+      let(:legal_aid_application) { create(:legal_aid_application, :with_positive_benefit_check_result) }
+      context 'provider has passported rights' do
+        before { provider.permissions << passported_permission }
+
+        it 'permits all actions' do
+          controller_actions.each do |action|
+            expect(subject).to permit(action)
+          end
+        end
+      end
+
+      context 'provider does not have passported rights' do
+        it 'does not permit any actions' do
+          controller_actions.each do |action|
+            expect(subject).not_to permit(action)
+          end
+        end
+      end
+    end
+
+    context 'application is non-passported' do
+      let(:legal_aid_application) { create(:legal_aid_application, :with_negative_benefit_check_result) }
+      context 'provider has non-passported rights' do
+        before { provider.permissions << non_passported_permission }
+
+        it 'permits all actions' do
+          controller_actions.each do |action|
+            expect(subject).to permit(action)
+          end
+        end
+      end
+
+      context 'provider does not have non-passported rights' do
+        it 'does not permit any actions' do
+          controller_actions.each do |action|
+            expect(subject).not_to permit(action)
+          end
+        end
+      end
+    end
   end
 end
