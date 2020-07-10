fa9d3a299246cb1140cf540e4113186c9251d3bf
willc-work <william.clarke@digital.justice.gov.uk>
2020-07-10 17:26:37 +0100

    failing tests for provider name

--- spec/requests/admin/roles_spec.rb
+++ spec/requests/admin/roles_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Admin::RolesController, type: :request do
   let(:admin_user) { create :admin_user }
-  let(:firm) { create :firm }
-  let(:firm2) { create :firm }
+  let(:firm) { create :firm, name: "Shotgun, Bobble & Dribble" }
+  let(:firm2) { create :firm, name: "Noodle, Legs & Co." }
+  let(:firm3) { create :firm }
   before { sign_in admin_user }
 
   describe 'GET index' do
@@ -17,6 +18,12 @@ RSpec.describe Admin::RolesController, type: :request do
     it 'displays correct heading' do
       subject
       expect(response.body).to include('Search for the Provider firm name')
+    end
+
+    it 'displays correct number of firms' do
+      subject
+      expect(response.body).to include('Shotgun, Bobble & Dribble')
+      expect(response.body).to include(:firm.name)
     end
   end
 end
